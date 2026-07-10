20738953f6f65ae69673d28ceab6493d81f206c5
AzarconAD <adamdaniel2801@gmail.com>
2026-07-10 22:07:58 +0800

    PATCH - qr_generator.py: wrong file name in the directory

--- app/utils/qr_generator.py
+++ app/utils/qr_generator.py
@@ -3,7 +3,7 @@ import qrcode
 from qrcode import ERROR_CORRECT_L
 from app.models.qr_data import QRData
 
-TEMP_DIR = "assets/temp_qr"
+TEMP_DIR = "assets/temp"
 os.makedirs(TEMP_DIR, exist_ok=True)
 
 def generate_qr_image(data: QRData) -> str:
@@ -14,13 +14,12 @@ def generate_qr_image(data: QRData) -> str:
         box_size=data.box_size,
         border=4,
     )
-    # Use the structured string
     qr.add_data(data.to_qr_string())
     qr.make(fit=True)
 
     img = qr.make_image(fill_color=data.fill_color, back_color=data.bg_color)
     
-    file_path = os.path.join(TEMP_DIR, "temp_qr_code.png")
+    file_path = os.path.join(TEMP_DIR, "temp_qr.png")
     img.save(file_path)
     
     return file_path
